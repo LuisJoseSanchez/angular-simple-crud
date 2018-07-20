5e1e1a78213398dddc68b10edb992644437856cd
Luis Jose Sanchez <luisjoseprofe@gmail.com>
2018-07-20 18:01:33 +0200

    Row user data is shown.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  users: User[];
+
+  constructor(private usersService: UsersService) {
+    this.users = usersService.getAllUsers();
+  }
 }
